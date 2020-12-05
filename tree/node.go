b0e5c430af4f48b1b7b0137610e19e5b1aec408b
vuhk <vuhk@ghtk.vn>
2020-12-05 11:56:24 +0700

    disjoint set

--- tree/node.go
+++ tree/node.go
@@ -1,7 +1,7 @@
 package tree
 
 type BinaryNode struct {
-	Key interface{}
-	Value interface{}
+	Key                 interface{}
+	Value               interface{}
 	Left, Right, Parent *BinaryNode
 }
